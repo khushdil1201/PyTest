f0e8feb73b8259aaabe5526cf9a11f7ff01dcb66
khushdil <khushdilikhtiyori@gmail.com>
2023-11-12 12:38:08 +0500

    Adding folder PyTest

---
+++ testing/test_entry_points.py
@@ -0,0 +1,7 @@
+import importlib.metadata
+
+
+def test_pytest_entry_points_are_identical():
+    dist = importlib.metadata.distribution("pytest")
+    entry_map = {ep.name: ep for ep in dist.entry_points}
+    assert entry_map["pytest"].value == entry_map["py.test"].value
